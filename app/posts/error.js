5205c799ca291551b828eb4dea8159f224012a6e
Vladislav <lycakvladislav@gmail.com>
2023-07-20 21:48:11 -0400

    add next js routing

---
+++ app/posts/error.js
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const error = () => {
+  return <div>error</div>;
+};
+
+export default error;
---
+++ app/posts/layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const layout = () => {
+  return <div>Navigate to top</div>;
+};
+
+export default layout;
