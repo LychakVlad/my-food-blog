8c84878cd7018424e5598ab97bc5944edeb15acb
Vladislav <lycakvladislav@gmail.com>
2023-07-23 21:51:26 -0400

    add nav sections

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,12 +6,37 @@ import { useEffect, useState } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         Logo
         <p className="logo_text"> Cook's Compass</p>
       </Link>
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-recipe" className="black_btn">
+              Create new recipe
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/profile.svg"
+                width={36}
+                height={36}
+                className="rounded-full"
+                alt="profile"
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
