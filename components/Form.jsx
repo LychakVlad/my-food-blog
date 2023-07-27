bd82219549c47cc8b0e2aed3b5640f6d348dd1a3
Vladislav <lycakvladislav@gmail.com>
2023-07-27 19:45:26 -0400

    add new inputs in create recipe page

--- components/Form.jsx
+++ components/Form.jsx
@@ -6,6 +6,40 @@ const Form = ({ type, post, submitting, handleSubmit }) => {
       <h1 className="head_text text-left">
         <span className="blue_gradient"> {type} Post</span>
       </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share your best recipes
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your recipe
+          </span>
+          <textarea
+            value={post.text}
+            onChange={(e) => setPost({ ...post, text: e.target.value })}
+            placeholder="Write your recipe here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag
+            <span className="font-normal"> (#dinner, #lunch, #breakfast)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+      </form>
     </section>
   );
 };
