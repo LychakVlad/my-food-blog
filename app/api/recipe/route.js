9f49cd0efa0c46800eca0fc52c1886e489206260
Vladislav <lycakvladislav@gmail.com>
2023-07-29 20:59:41 -0400

    add fetch data api

---
+++ app/api/recipe/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from '@utils/database';
+import Text from '@models/recipe';
+
+export const GET = async (request) => {
+  try {
+    await connectToDB();
+
+    const recipes = await Text.find({}).populate('creator');
+
+    return new Response(JSON.stringify(recipes), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to fetch all recipes', { status: 500 });
+  }
+};
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,20 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import RecipeCard from './RecipeCard';
 
-const RecipeCardList = ({ card, handleTagClick }) => {
-  return <div className="mt-16 prompt_layout"></div>;
+const RecipeCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <RecipeCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Feed = () => {
@@ -14,7 +25,7 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const resonse = await fetch('/api/recipe');
+      const response = await fetch('/api/recipe');
       const data = await response.json();
 
       setPosts(data);
