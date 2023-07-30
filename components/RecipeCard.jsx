4a255bb9a293cc022a8d1661a7c54b47b112226c
Vladislav <lycakvladislav@gmail.com>
2023-07-29 21:43:50 -0400

    update recipe card component

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+'use client';
 
-const RecipeCard = () => {
-  return <div>RecipeCard</div>;
+import Image from 'next/image';
+
+const RecipeCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={() => {}}></div>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeCard;
