0fca334ac20556474977b432da301e1d136208a3
Vladislav <lycakvladislav@gmail.com>
2023-07-31 21:42:16 -0400

    update my profile component

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,37 @@
+'use client';
+
 import React from 'react';
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from '@components/profile';
+import { useSession } from 'next-auth/react';
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/recipe`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
