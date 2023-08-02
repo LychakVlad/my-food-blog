2fde2670d1fa031e643843f5657b6889ac219e43
Vladislav <lycakvladislav@gmail.com>
2023-08-01 21:44:29 -0400

    add new buttons on recipe cards

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,7 @@ const MyProfile = () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
-      console.log(response);
+      console.log(data);
 
       setPosts(data);
     };
--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -2,9 +2,14 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
 
 const RecipeCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.text);
@@ -51,6 +56,23 @@ const RecipeCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
