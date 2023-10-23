88940d7bf3f2f95aef4c116b978006467d16a78c
Vladislav <lycakvladislav@gmail.com>
2023-10-23 07:18:30 -0400

    add styling to recipe page

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -4,9 +4,38 @@ import { IPost } from '../types/recipe.interface';
 
 const Recipe = ({ post }: { post: IPost }) => {
   return (
-    <div>
+    <div className="max-w-2xl">
+      <h1 className="recipe_title">{post.title} </h1>
+      <p className="desc my-10">{post.text}</p>
+
+      <div className="relative  max-w-[672px] overflow-hidden h-[500px] flex items-center ">
+        {' '}
+        <Image
+          src={'/assets/images/recipe-photo.jpeg'}
+          alt="food_image"
+          width={672}
+          height={500}
+          className="object-cover object-center  absolute"
+        />
+      </div>
+      <h2 className="recipe_semi-title my-10">Ingredients</h2>
+      <p>
+        {post.ingredients.map((ingredient: string, index: number) => (
+          <span>{ingredient}</span>
+        ))}
+      </p>
+      <h2 className="recipe_semi-title my-10">Directions</h2>
+
+      {post.steps.map((step: string, index: number) => (
+        <div className="mb-10">
+          <h4 className="recipe_step-title mb-2"> {'Step ' + (index + 1)}</h4>
+
+          <p>{step}</p>
+        </div>
+      ))}
+
+      <p>#{post.tag}</p>
       <h3>{post?.creator?.username}</h3>
-      <h3>{post?.creator?.email}</h3>
       <Image
         src={
           post?.creator?.image
@@ -18,22 +47,6 @@ const Recipe = ({ post }: { post: IPost }) => {
         height={60}
         className="rounded-full object-contain"
       />
-
-      <h1>Title: {post.title} </h1>
-      <h2>Text: {post.text}</h2>
-      <h2>Ingredients</h2>
-      <p>
-        {post.ingredients.map((ingredient: string) => (
-          <span>{ingredient}</span>
-        ))}
-      </p>
-      <h2>Steps</h2>
-      <p>
-        {post.steps.map((step: string) => (
-          <span>{step}</span>
-        ))}
-      </p>
-      <p>#{post.tag}</p>
     </div>
   );
 };
