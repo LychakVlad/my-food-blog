580ba3f8156a3bb7b55b25fecfad71166986cd11
Vladislav <lycakvladislav@gmail.com>
2023-10-24 06:28:27 -0400

    add styling to recipe page

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -6,6 +6,8 @@ const Recipe = ({ post }: { post: IPost }) => {
   return (
     <div className="max-w-2xl">
       <h1 className="recipe_title">{post.title} </h1>
+      <h3 className="mt-2 text-lg">Recipe by {post?.creator?.username}</h3>
+      <h3 className="mt-2 text-lg">#{post.tag}</h3>
       <p className="desc my-10">{post.text}</p>
 
       <div className="relative  max-w-[672px] overflow-hidden h-[500px] flex items-center ">
@@ -19,34 +21,24 @@ const Recipe = ({ post }: { post: IPost }) => {
         />
       </div>
       <h2 className="recipe_semi-title my-10">Ingredients</h2>
-      <p>
-        {post.ingredients.map((ingredient: string, index: number) => (
-          <span>{ingredient}</span>
+      <ul>
+        {post.ingredients.map((ingredient: string) => (
+          <li className="mb-2 pl-5 relative">
+            <span className="before:absolute before:bg-gray-500 before:w-1.5 before:h-1.5 before:rounded-full before:left-1 before:top-2.5"></span>
+            {ingredient}
+          </li>
         ))}
-      </p>
+      </ul>
       <h2 className="recipe_semi-title my-10">Directions</h2>
-
-      {post.steps.map((step: string, index: number) => (
-        <div className="mb-10">
-          <h4 className="recipe_step-title mb-2"> {'Step ' + (index + 1)}</h4>
-
-          <p>{step}</p>
-        </div>
-      ))}
-
-      <p>#{post.tag}</p>
-      <h3>{post?.creator?.username}</h3>
-      <Image
-        src={
-          post?.creator?.image
-            ? post.creator.image
-            : '/assets/icons/profile-undefined.svg'
-        }
-        alt="user_image"
-        width={60}
-        height={60}
-        className="rounded-full object-contain"
-      />
+      <ol>
+        {post.steps.map((step: string, index: number) => (
+          <li className="mb-10">
+            {' '}
+            <h4 className="recipe_step-title mb-2"> {'Step ' + (index + 1)}</h4>
+            <p>{step}</p>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
