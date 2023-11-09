371036aad1193fdbb594d565b57605b225789e4a
Vladislav <lycakvladislav@gmail.com>
2023-11-09 07:19:01 -0500

    fix api

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -2,6 +2,7 @@ import { connectToDB } from '../../../utils/database';
 import Recipe from '../../../models/recipe';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export const GET = async (request: NextRequest, { params }: any) => {
   try {
     await connectToDB();
--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -4,7 +4,7 @@ import { IPost } from '../../../types/recipe.interface';
 interface Params {
   id: string;
 }
-export async function dynamicData() {
+export async function generateStaticParams() {
   const posts = await fetch(`${process.env.NEXTAUTH_URL}/api/recipe`);
   const data = await posts.json();
 
@@ -23,5 +23,3 @@ export default async function RecipePage({ params }: { params: Params }) {
   const recipe = await getRecipe(params.id);
   return <Recipe post={recipe} />;
 }
-
-export const dynamic = 'force-dynamic';
