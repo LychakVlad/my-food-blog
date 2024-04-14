629a7340f5a33692b52dacf041612b69aee6db1c
Vladislav <lycakvladislav@gmail.com>
2024-04-14 18:27:19 -0400

    add console log

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -1,18 +1,19 @@
-import { connectToDB } from '../../../utils/database';
-import Recipe from '../../../models/recipe';
-import { NextRequest, NextResponse } from 'next/server';
+import { connectToDB } from "../../../utils/database";
+import Recipe from "../../../models/recipe";
+import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export const GET = async (request: NextRequest, { params }: any) => {
   try {
     await connectToDB();
 
     const recipes = await Recipe.find({})
-      .populate('creator')
-      .select('-hashedPassword');
+      .populate("creator")
+      .select("-hashedPassword");
 
-    return new NextResponse(JSON.stringify(recipes), { status: 200 });
+    return NextResponse.json(recipes, { status: 200 });
   } catch (error) {
-    return new NextResponse('Failed to fetch all recipes', { status: 500 });
+    console.log(error);
+    return new NextResponse("Failed to fetch all recipes", { status: 500 });
   }
 };
